623efe0dedefba02b2a1801547f78fd5ee580dba
noya.lin <noya.lin@voyageone.cn>
2020-01-17 17:47:46 +0800

    first commit wechat

---
+++ miniprogram/pages/workStaging/workStaging.js
@@ -0,0 +1,24 @@
+// pages/workStaging/workStaging.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    pendingList: [{ name: '待办方案', value: 100 }, { name: '已办课题', value: 99 }, { name: '我的请求', value: 1 }, { name: '我的请求', value: 1 }],
+    meetingList: [{title:'会议主题', content:'sdsds'},{title:'会议时间', content:'sdsds'},{title:'会议地点', content:'sdsds'}]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
