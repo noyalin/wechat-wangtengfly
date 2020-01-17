c27325b3893c7bbd0bbd58e5ce62a0d9d8361c6a
noya.lin <noya.lin@voyageone.cn>
2020-01-17 18:49:36 +0800

    update

--- miniprogram/pages/workStaging/workStaging.js
+++ miniprogram/pages/workStaging/workStaging.js
@@ -12,8 +12,17 @@ Component({
    */
   data: {
     pendingList: [{ name: '待办方案', value: 100 }, { name: '已办课题', value: 99 }, { name: '我的请求', value: 1 }, { name: '我的请求', value: 1 }],
-    meetingList: [{title:'会议主题', content:'sdsds'},{title:'会议时间', content:'sdsds'},{title:'会议地点', content:'sdsds'}],
-    processList: [{title: '差旅报销kjhjkh'},{title: '差旅报销'},{title: '差旅报销'},{title: '差旅报销'},{title: '差旅报销'},{title: '差旅报销'},{title: '差旅报销'},{title: '差旅报销'}]
+    meetingList: [{ title: '会议主题', content: 'sdsds' }, { title: '会议时间', content: 'sdsds' }, { title: '会议地点', content: 'sdsds' }],
+    processList: [
+      { title: '差旅报销kjhjkh' },
+      { title: '出差报备' },
+      { title: '请假流程' },
+      { title: '对外付款' },
+      { title: '员工入职' },
+      { title: '员工离职' },
+      { title: '请假流程' },
+      { title: '招聘申请' }
+    ]
   },
 
   /**
